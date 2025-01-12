24e3227342564f81b64fd5ecfd4e9468e57ff544
VegirowthuManojTarun <manojtarun934.com>
2025-01-12 12:19:12 +0530

    environment variables created

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import Cookies from "js-cookie";
 
-const supabaseUrl = "https://ngquhdgmeqywicptbfcv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ncXVoZGdtZXF5d2ljcHRiZmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1MDI1MDIsImV4cCI6MjA1MjA3ODUwMn0.Zvp1NpGDq8tJIWsaehUoipO5YjZpAOVBza2pJ5V_4Gw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-
 
 export default supabase;
